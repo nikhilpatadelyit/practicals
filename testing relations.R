6e0bb5bf15a1f3e52dc8a1f4763989c10926d1d4
unknown <l00157070@student.lyit.ie>
2021-05-04 14:33:35 +0100

    Initial commit of testing relations

---
+++ testing relations.R
@@ -0,0 +1,20 @@
+# Use statistical methods to examine
+# the relationship between to variables of interest
+
+# BEAVERS DATASET
+# contains the body temp of 4 beavers captures every 10 mins
+# over a day
+# we want to examine difference in average body temp
+# to evaluate whether body temp is affected by activity
+
+# H0: mean beavers temp. is not affecetd by activity
+# H1: mean beaver temp. is affected by activity
+
+?beavers
+str(beaver2)
+
+# vars we'll need
+# We'll evaluate beaver temp and activity
+# temp = continous variable
+# activty - categorical dicotomous variable
+
